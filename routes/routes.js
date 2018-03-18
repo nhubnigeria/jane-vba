0cfe192a81f80fbf4d01277d21b061259a64a813
Victor Olowe <vikie95@gmail.com>
2018-03-18 14:53:33 +0100

    🎉 initial commit

---
+++ routes/routes.js
@@ -0,0 +1,42 @@
+'use strict';
+/**
+ * Module dependencies
+ */
+//=============================================================================
+const
+  express = require('express'),
+  router = express.Router(),
+  talkToBot = require('../utils/ChatBot');
+//=============================================================================
+/**
+ * Module variables
+ */
+//=============================================================================
+//=============================================================================
+/**
+ * Routes
+ */
+//=============================================================================
+router.get('/welcome', async (req, res) => {
+  try {
+    let responseFromBot = await talkToBot(null)
+    res.status(200).json({ responseFromBot })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+})
+router.post('/send_message', async (req, res) => {
+  try {
+    let responseFromBot = await talkToBot(req.body.input)
+    res.status(200).json({ responseFromBot })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+})
+//=============================================================================
+/**
+ * Export Module
+ */
+//=============================================================================
+module.exports = router;
+//=============================================================================
